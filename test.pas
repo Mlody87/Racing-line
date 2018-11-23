b1a06c948cd55476504c45d9d178db424f97d8d9
Mlody87 <kabatpawel@gmail.com>
2018-11-23 13:19:40 +0100

    Update test.pas

--- test.pas
+++ test.pas
@@ -1,3 +1,61 @@
+program matrix;
+type
+   TMy_arr = array[0..7,0..7] of string;
+  
+
+function ArrayRotation(arr : TMy_arr): TMy_arr;
+var
+X,Y:integer;
+Temp:string;
+begin
+for X := 0 to 3 do
+  for Y := 0 to 7 do
+  begin
+    Temp := arr[X, Y];
+    ArrayRotation[X, Y] := arr[7 - X, 7 - Y];
+    ArrayRotation[7 - X, 7 - Y] := Temp;
+  end;
+end;
+   
+var
+a : TMy_arr =
+    (('A8','B8','C8','D8','E8','F8','G8','H8'),
+    ('A7','B7','C7','D7','E7','F7','G7','H7'),
+    ('A6','B6','C6','D6','E6','F6','G6','H6'),
+    ('A5','B5','C5','D5','E5','F5','G5','H5'),
+    ('A4','B4','C4','D4','E4','F4','G4','H4'),
+    ('A3','B3','C3','D3','E3','F3','G3','H3'),
+    ('A2','B2','C2','D2','E2','F2','G2','H2'),
+    ('A1','B1','C1','D1','E1','F1','G1','H1'));
+i,j:integer;
+
+begin
+
+
+for i:=0 to 7 do
+begin
+writeLn('');
+    for j:=0 to 7 do
+        write(a[i,j]+' ');
+end;
+
+writeLn('');
+writeLn('');
+
+a := ArrayRotation(a);
+
+for i:=0 to 7 do
+begin
+writeLn('');
+    for j:=0 to 7 do
+        write(a[i,j]+' ');
+end;
+
+end.
+
+
+
+
 POLA : array[1..8,1..8] of string =
     (('A8','B8','C8','D8','E8','F8','G8','H8'),
     ('A7','B7','C7','D7','E7','F7','G7','H7'),
