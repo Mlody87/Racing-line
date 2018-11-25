f022cfccf9a9dc2625311a2085cecee3d6518661
Mlody87 <kabatpawel@gmail.com>
2018-11-25 22:20:08 +0100

    Update test.pas

--- test.pas
+++ test.pas
@@ -1,75 +1,3 @@
-    {--- rysowanie planszy ---}
-
-  
-
-  {--- rysujemy mozliwe ruchy jezeli takie sa---}
-
-if Length(TablicaRuchow)>0 then
-begin
-
-     for a:=0 to Length(TablicaRuchow)-1 do
-     begin
-           pole:=ZnajdzIJbyPole(TablicaRuchow[a]);
-
-           PaintBox1.Canvas.brush.Color:=$00AAFFFF;
-
-            t.Left:=(80*(pole.y-1));
-            t.Top:=(80*(pole.x-1));
-            t.Right:=(80*(pole.y-1))+81;
-            t.Bottom:=(80*(pole.x-1))+81;
-
-             PaintBox1.Canvas.rectangle(t);
-     end;
-
-end;
-
-{-- kolorujey szacha jezeli jest --}
-     if KolorowanieSzach.ok=true then
-     begin
-       PaintBox1.Canvas.brush.Color:=$006A67FA;
-
-        t.Left:=(80*(KolorowanieSzach.Pole.y-1));
-        t.Top:=(80*(KolorowanieSzach.Pole.x-1));
-        t.Right:=(80*(KolorowanieSzach.Pole.y-1))+81;
-        t.Bottom:=(80*(KolorowanieSzach.Pole.x-1))+81;
-
-         PaintBox1.Canvas.rectangle(t);
-
-     end;
-
-{---}
-
-  PaintBox1.Canvas.Pen.Color := clBlack;
-
-     PaintBox1.Canvas.MoveTo(0,0);
-     PaintBox1.Canvas.LineTo(640,0);
-
-     PaintBox1.Canvas.MoveTo(640,0);
-     PaintBox1.Canvas.LineTo(640,640);
-
-     PaintBox1.Canvas.MoveTo(0,0);
-     PaintBox1.Canvas.LineTo(0,640);
-
-     PaintBox1.Canvas.MoveTo(0,640);
-     PaintBox1.Canvas.LineTo(640,640);
-
-
-{----    rysowanie figur -----}
-
-      with PaintBox1.Canvas do
-   begin
-   for i:=1 to 8 do
-   begin
-       for j:=1 to 8 do
-       begin
-       if Board[i,j]<>nil then begin
-             Draw(Board[i,j].pozycja.X+5, Board[i,j].pozycja.Y+5, Board[i,j].obraz);
-       end;
-       end;
-       end;
-   end;         
-
-
 unit Board;
 
 {$mode objfpc}{$H+}
@@ -77,20 +5,25 @@ unit Board;
 interface
 
 uses
-  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, ExtCtrls, BGRABitmap, BGRASVG, BGRABitmapTypes;
 
 type
-  
+
   TPiece = record
-  Piece:integer;
+  Piece:string;
   Color:string;
-  Image:TPortableNetworkGraphic;
+  Image: TBGRASVG;
   MoveCount:integer;
   Field:string;
-  Width:integer;
-  Height:integer;
   end;
-  
+
+  TDAD = record
+  DAD:boolean;
+  DADCordsIJ:TPoint;
+  DADCordsXY:TPoint;
+  DADBoardPoint:TPoint;
+  end;
+
   TBoardDesc = array[0..7,0..7] of string;
   TPieces = array[0..8] of string;
   TBoardPieces = array[0..7,0..7] of TPiece;
@@ -99,36 +32,35 @@ type
   private
     { Private declarations }
     FBottomColor:String;
-    BoardDesc : TBoardDesc =
-    (('A8','B8','C8','D8','E8','F8','G8','H8'),
-    ('A7','B7','C7','D7','E7','F7','G7','H7'),
-    ('A6','B6','C6','D6','E6','F6','G6','H6'),
-    ('A5','B5','C5','D5','E5','F5','G5','H5'),
-    ('A4','B4','C4','D4','E4','F4','G4','H4'),
-    ('A3','B3','C3','D3','E3','F3','G3','H3'),
-    ('A2','B2','C2','D2','E2','F2','G2','H2'),
-    ('A1','B1','C1','D1','E1','F1','G1','H1'));
-    Pieces : TPieces =
-    ('Pawn', 'Roock', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rock');
-    PiecesBlack : TPieces =
-    ('Pawn', 'Roock', 'Knight', 'Bishop', 'King', 'Queen', 'Bishop', 'Knight', 'Rock');
     Board:TBoardPieces;
+    DAD:TDAD;
   protected
     { Protected declarations }
+    BoardDesc : TBoardDesc;
+        Pieces : TPieces;
+        PiecesBlack : TPieces;
+
     procedure Paint(); override;
     function BoardRotation(arr : TBoardDesc): TBoardDesc;
     function BoardRotationPieces(arr : TBoardPieces): TBoardPieces;
     function FieldSize():integer;
     procedure DrawBoard();
     procedure DrawLines();
+    procedure DrawPosition();
     procedure SetStartPosition();
+    procedure SetVariables();
+    function GetFieldIJ(X,Y:integer):TPoint;
+    function GetFieldXY(X,Y:integer):TPoint;
   public
     { Public declarations }
     constructor Create(AOwner : TComponent); override;
     destructor Destroy; override;
+    procedure SetBottomColor(botcolor:string);
+    procedure MouseDown(Button: TMouseButton;Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseUp(Button: TMouseButton;Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseMove(Shift: TShiftState; X,Y: Integer); override;
   published
     { Published declarations }
-    property BottomColor : String read FBottomColor write FBottomColor;
   end;
 
 procedure Register;
@@ -138,6 +70,57 @@ implementation
 procedure Register;
 begin
   RegisterComponents('Additional',[TBoard]);
+end;
+
+function TBoard.GetFieldIJ(X,Y:integer):TPoint;
+begin
+
+  GetFieldIJ.X:=(X div FieldSize())+1;
+  GetFieldIJ.Y:=(Y div FieldSize())+1;
+
+end;
+
+
+function TBoard.GetFieldXY(X,Y:integer):TPoint;
+var
+Point:TPoint;
+begin
+
+  Point:=GetFieldIJ(X,Y);
+
+  GetFieldXY.X:=(Point.X)*FieldSize();
+  GetFieldXY.Y:=(Point.Y)*FieldSize();
+
+end;
+
+procedure TBoard.MouseDown(Button: TMouseButton;Shift: TShiftState; X, Y: Integer);
+begin
+  inherited;
+
+  DAD.DAD:=true;
+
+  DAD.DADCordsIJ:=GetFieldIJ(X,Y);
+
+  DAD.DADCordsXY:=GetFieldXY(X,Y);
+
+  DAD.DADBoardPoint:=Point(X,Y);
+
+end;
+
+procedure TBoard.MouseMove(Shift: TShiftState; X,Y: Integer);
+begin
+  inherited;
+
+
+
+end;
+
+procedure TBoard.MouseUp(Button: TMouseButton;Shift: TShiftState; X, Y: Integer);
+begin
+  inherited;
+
+
+
 end;
 
 function TBoard.BoardRotation(arr : TBoardDesc): TBoardDesc;
@@ -154,7 +137,7 @@ for X := 0 to 3 do
   end;
 end;
 
-function BoardRotationPieces(arr : TBoardPieces): TBoardPieces;
+function TBoard.BoardRotationPieces(arr : TBoardPieces): TBoardPieces;
 var
 X,Y:integer;
 Temp:TPiece;
@@ -176,7 +159,7 @@ end;
 procedure TBoard.DrawBoard();
 var
 iswhite:boolean;
-i,j,FieldSize:integer;
+i,j:integer;
 field:TRect;
 begin
 
@@ -214,15 +197,15 @@ begin
 
      Canvas.rectangle(field);
 
-      if white then
+      if iswhite then
       begin
-         white:=false;
+         iswhite:=false;
       end
       else
       begin
-         white:=true;
+         iswhite:=true;
       end;
-      
+
       end;
    end;
 end;
@@ -255,60 +238,115 @@ begin
 for i:=0 to 7 do
 begin
   //set white Pawns
-  Board[7,i].Piece:=Pieces[0];
-  Board[7,i].Color:='white';
-  Board[7,i].Image:=TPortableNetworkGraphic.Create;
-  Board[7,i].Image:=LoadFromFile('img/'+Pieces[0]+'White.png');
-  Board[7,i].MoveCount:=0;
-  Board[7,i].Field:=BoardDesc[7,i];
-  Board[7,i].Width:=50;
-  Board[7,i].Height:=50;
+  Board[i,6].Piece:=Pieces[0];
+  Board[i,6].Color:='white';
+  Board[i,6].Image:=TBGRASVG.Create('C:\Users\Mlody\SzachownicaKomponent\img\'+Pieces[0]+'White.svg');
+  Board[i,6].MoveCount:=0;
+  Board[i,6].Field:=BoardDesc[i,6];
   //set black Pawns
-  Board[1,i].Piece:=Pieces[0];
-  Board[1,i].Color:='black';
-  Board[1,i].Image:=TPortableNetworkGraphic.Create;
-  Board[1,i].Image:=LoadFromFile('img/'+Pieces[0]+'Black.png');
-  Board[1,i].MoveCount:=0;
-  Board[1,i].Field:=BoardDesc[1,i];
-  Board[1,i].Width:=50;
-  Board[1,i].Height:=50;
+  Board[i,1].Piece:=Pieces[0];
+  Board[i,1].Color:='black';
+  Board[i,1].Image:=TBGRASVG.Create('C:\Users\Mlody\SzachownicaKomponent\img\'+Pieces[0]+'Black.svg');
+  Board[i,1].MoveCount:=0;
+  Board[i,1].Field:=BoardDesc[i,1];
 end;
 
 //set white Pices
 for i:=0 to 7 do
 begin
-  Board[8,i].Piece:=Pieces[i+1];
-  Board[8,i].Color:='white';
-  Board[8,i].Image:=TPortableNetworkGraphic.Create;
-  Board[8,i].Image:=LoadFromFile('img/'+Pieces[i+1]+'White.png');
-  Board[8,i].MoveCount:=0;
-  Board[8,i].Field:=BoardDesc[8,i];
-  Board[8,i].Width:=50;
-  Board[8,i].Height:=50;
-//set black Pices  
-  Board[0,i].Piece:=PiecesBlack[i+1];
-  Board[0,i].Color:='black';
-  Board[0,i].Image:=TPortableNetworkGraphic.Create;
-  Board[0,i].Image:=LoadFromFile('img/'+Pieces[i+1]+'Black.png');
-  Board[0,i].MoveCount:=0;
-  Board[0,i].Field:=BoardDesc[0,i];
-  Board[0,i].Width:=50;
-  Board[0,i].Height:=50;
+  Board[i,7].Piece:=Pieces[i+1];
+  Board[i,7].Color:='white';
+  Board[i,7].Image:=TBGRASVG.Create('C:\Users\Mlody\SzachownicaKomponent\img\'+Pieces[i+1]+'White.svg');
+  Board[i,7].MoveCount:=0;
+  Board[i,7].Field:=BoardDesc[i,7];
+//set black Pices
+  Board[i,0].Piece:=PiecesBlack[i+1];
+  Board[i,0].Color:='black';
+  Board[i,0].Image:=TBGRASVG.Create('C:\Users\Mlody\SzachownicaKomponent\img\'+Pieces[i+1]+'Black.svg');
+  Board[i,0].MoveCount:=0;
+  Board[i,0].Field:=BoardDesc[i,0];
 end;
 
+end;
+
+procedure TBoard.SetVariables();
+begin
+
+FBottomColor := 'white';
+
+Pieces[0]:='Pawn'; Pieces[1]:='Rook'; Pieces[2]:='Knight'; Pieces[3]:='Bishop';
+Pieces[4]:='Queen'; Pieces[5]:='King'; Pieces[6]:='Bishop'; Pieces[7]:='Knight'; Pieces[8]:='Rook';
+
+BoardDesc[0,0]:='A8';BoardDesc[0,1]:='B8';BoardDesc[0,2]:='C8';BoardDesc[0,3]:='D8';BoardDesc[0,4]:='E8';BoardDesc[0,5]:='F8';BoardDesc[0,6]:='G8';BoardDesc[0,7]:='H8';
+BoardDesc[1,0]:='A7';BoardDesc[1,1]:='B7';BoardDesc[1,2]:='C7';BoardDesc[1,3]:='D7';BoardDesc[1,4]:='E7';BoardDesc[1,5]:='F7';BoardDesc[1,6]:='G7';BoardDesc[1,7]:='H7';
+BoardDesc[2,0]:='A6';BoardDesc[2,1]:='B6';BoardDesc[2,2]:='C6';BoardDesc[2,3]:='D6';BoardDesc[2,4]:='E6';BoardDesc[2,5]:='F6';BoardDesc[2,6]:='G6';BoardDesc[2,7]:='H6';
+BoardDesc[3,0]:='A5';BoardDesc[3,1]:='B5';BoardDesc[3,2]:='C5';BoardDesc[3,3]:='D5';BoardDesc[3,4]:='E5';BoardDesc[3,5]:='F5';BoardDesc[3,6]:='G5';BoardDesc[3,7]:='H5';
+BoardDesc[4,0]:='A4';BoardDesc[4,1]:='B4';BoardDesc[4,2]:='C4';BoardDesc[4,3]:='D4';BoardDesc[4,4]:='E4';BoardDesc[4,5]:='F4';BoardDesc[4,6]:='G4';BoardDesc[4,7]:='H4';
+BoardDesc[5,0]:='A3';BoardDesc[5,1]:='B3';BoardDesc[5,2]:='C3';BoardDesc[5,3]:='D3';BoardDesc[5,4]:='E3';BoardDesc[5,5]:='F3';BoardDesc[5,6]:='G3';BoardDesc[5,7]:='H3';
+BoardDesc[6,0]:='A2';BoardDesc[6,1]:='B2';BoardDesc[6,2]:='C2';BoardDesc[6,3]:='D2';BoardDesc[6,4]:='E2';BoardDesc[6,5]:='F2';BoardDesc[6,6]:='G2';BoardDesc[6,7]:='H2';
+BoardDesc[7,0]:='A1';BoardDesc[7,1]:='B1';BoardDesc[7,2]:='C1';BoardDesc[7,3]:='D1';BoardDesc[7,4]:='E1';BoardDesc[7,5]:='F1';BoardDesc[7,6]:='G1';BoardDesc[7,7]:='H1';
+
+PiecesBlack[0]:='Pawn';PiecesBlack[1]:='Rook';PiecesBlack[2]:='Knight';PiecesBlack[3]:='Bishop';
+PiecesBlack[4]:='King';PiecesBlack[4]:='Queen';PiecesBlack[5]:='Bishop';PiecesBlack[6]:='Knight';PiecesBlack[7]:='Rook';
+
+end;
+
+
+procedure TBoard.DrawPosition();
+var
+i,j:integer;
+lockX,lockY:integer;
+BMP:TBGRABitmap;
+begin
+
+   for i:=0 to 7 do
+   begin
+       for j:=0 to 7 do
+       begin
+       if Board[i,j].Image<>nil then
+       begin
+         lockX:=(i*FieldSize())+5;
+         lockY:=(j*FieldSize())+5;
+
+         BMP:=TBGRABitmap.Create;
+
+         BMP.SetSize(FieldSize()-5,FieldSize()-5);
+
+         Board[i,j].Image.StretchDraw(BMP.Canvas2D, taCenter, tlCenter, 0,0,FieldSize()-10,FieldSize()-10);
+         Canvas.Draw(lockX, lockY, BMP.Bitmap);
+
+         BMP.Free;
+
+       end;
+   end;
+
+end;
+
+end;
 
 constructor TBoard.Create(AOwner : TComponent);
 begin
   inherited Create(AOwner);
-  
+  Self.Width:=500;
+Self.Height:=500;
+  SetVariables();
   SetStartPosition();
-  
-  if (FBottomColor='black') then
+
+end;
+
+procedure TBoard.SetBottomColor(botcolor:string);
+begin
+
+   if (FBottomColor<>botcolor) then
       begin
       BoardDesc := BoardRotation(BoardDesc);
       Board := BoardRotationPieces(Board);
       end;
-  
+
+   FBottomColor:=botcolor;
+
+   Self.Repaint;
+
 end;
 
 destructor TBoard.Destroy;
@@ -318,11 +356,10 @@ end;
 
 procedure TBoard.Paint();
 begin
-
 DrawBoard();
 DrawLines();
+DrawPosition();
 
 end;
 
 end.
-
