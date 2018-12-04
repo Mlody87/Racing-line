593a1549c38a72ba921f9de7eb78d078a5292365
Mlody87 <kabatpawel@gmail.com>
2018-12-04 12:52:04 +0100

    Update new.pas

--- new.pas
+++ new.pas
@@ -1,1 +1,18 @@
+TPossibleMoves = array of string;
+PossibleMoves:TPossibleMoves;
 
+function TBoard.RookMoves(field:TPoint):TPossibleMoves;
+var
+color:string;
+i:integer;
+begin
+color:=Board[field.x,field.y].color;
+
+for i:=field.x to 7 do
+  begin
+    
+  end;
+
+
+
+end;
